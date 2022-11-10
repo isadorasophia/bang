0dd1dc380ce0d67c3222f483b6c66550fa636ea0
bonnibel <isaecia@gmail.com>
2022-11-09 18:52:11 -0800

    Add interface for transform components.

---
+++ src/Components/ITransformComponent.cs
@@ -0,0 +1,7 @@
+﻿namespace Bang.Components
+{
+    /// <summary>
+    /// Interface implemented by components that rely on a transform within a world.
+    /// </summary>
+    public interface ITransformComponent : IParentRelativeComponent { }
+}
