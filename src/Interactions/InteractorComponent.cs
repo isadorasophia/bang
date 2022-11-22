96b44cd70753d0270b1cfece986b4dee175dcbf7
bonnibel <isaecia@gmail.com>
2022-11-22 01:51:40 -0800

    Change to interactor.

--- src/Interactions/InteractorComponent.cs
+++ src/Interactions/InteractorComponent.cs
@@ -5,5 +5,5 @@ namespace Bang.Interactions
     /// <summary>
     /// This component is to signalize that an entity is able to interact with other objects.
     /// </summary>
-    public struct InteractorComponent : IComponent { }
+    public readonly struct InteractorComponent : IComponent { }
 }
