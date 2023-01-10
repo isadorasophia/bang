ce50b5216688a6008195dde80611f24ee3d08448
bonnibel <isaecia@gmail.com>
2023-01-09 22:32:05 -0800

    Improve error message.

--- src/Diagnostics/Assert.cs
+++ src/Diagnostics/Assert.cs
@@ -2,6 +2,13 @@
 {
     public static class Assert
     {
+        /*
+         * Hi! 
+         * (╯°□°)╯︵ ┻━┻
+         * I am so sorry that you hit this spot.
+         * You can check the call stack to see what of unexpected happened.
+         * ┬─┬ノ( º _ ºノ)
+         * */
         public static void Verify(bool condition, string text)
         {
             if (!condition)
