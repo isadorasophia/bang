dd4ae9d00f937a3295602599e43945b27f17e690
William Barbosa <heytherewill@gmail.com>
2023-09-16 09:12:23 +0200

    🛠️ Add readonly struct code fix

---
+++ src/Bang.Analyzers/CodeFixProviders/ReadonlyStructCodeFixProvider.cs
@@ -0,0 +1,66 @@
+using System.Collections.Immutable;
+using System.Composition;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Bang.Analyzers.Fixes;
+
+[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(ReadonlyStructCodeFixProvider)), Shared]
+public class ReadonlyStructCodeFixProvider : CodeFixProvider
+{
+    public sealed override ImmutableArray<string> FixableDiagnosticIds
+        => ImmutableArray.Create(
+            Diagnostics.Components.ComponentsMustBeReadonly.Id,
+            Diagnostics.Messages.MessagesMustBeReadonly.Id
+        );
+
+    public sealed override FixAllProvider GetFixAllProvider()
+        => WellKnownFixAllProviders.BatchFixer;
+
+    public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+    {
+        var root = await context.Document
+            .GetSyntaxRootAsync(context.CancellationToken)
+            .ConfigureAwait(false);
+
+        if (root is null)
+            return;
+
+        foreach (var diagnostic in context.Diagnostics)
+        {
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+            if (root.FindToken(diagnosticSpan.Start).Parent is not TypeDeclarationSyntax typeDeclaration)
+                return;
+
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: CodeFixes.ReadonlyStruct.Title,
+                    createChangedDocument: c => MakeStructReadonly(context.Document, typeDeclaration, c),
+                    equivalenceKey: nameof(CodeFixes.ReadonlyStruct)),
+                diagnostic
+            );
+        }
+    }
+
+    private static async Task<Document> MakeStructReadonly(
+        Document document,
+        TypeDeclarationSyntax recordDeclarationSyntax,
+        CancellationToken cancellationToken
+    )
+    {
+        // Add a readonly modifier to the struct.
+        var readonlyToken = SyntaxFactory.Token(SyntaxKind.ReadOnlyKeyword);
+        var readonlyStructDeclarationSyntax = recordDeclarationSyntax.AddModifiers(readonlyToken);
+
+        // Replace the old struct declaration with the new, readonly, one.
+        var oldRoot = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+        if (oldRoot is null)
+            return document;
+
+        var newRoot = oldRoot.ReplaceNode(recordDeclarationSyntax, readonlyStructDeclarationSyntax);
+        return document.WithSyntaxRoot(newRoot);
+    }
+}
---
+++ src/Bang.Analyzers/CodeFixes.cs
@@ -0,0 +1,9 @@
+namespace Bang.Analyzers;
+
+public static class CodeFixes
+{
+    public static class ReadonlyStruct
+    {
+        public const string Title = "Convert to readonly struct";
+    }
+}
