2a292b40d1b36e7523a34101374a70858a146399
bonnibel <isaecia@gmail.com>
2023-09-17 13:54:28 -0700

    Fix warning for shipped version on Analyzers.

--- src/Bang.Analyzers/AnalyzerReleases.Shipped.md
+++ src/Bang.Analyzers/AnalyzerReleases.Shipped.md
@@ -1,16 +1,18 @@
+## Release 0.0.3
+
 ### New Rules
 
-| Rule ID  | Category | Severity | Notes                                     |
-|----------|----------|----------|-------------------------------------------|
-| BANG0001 | Usage    | Error    | Classes cannot be components.             |
-| BANG0002 | Usage    | Error    | Components must be declared as readonly.  |
-| BANG1001 | Usage    | Error    | System requires FilterAttribute.          |
-| BANG1002 | Usage    | Error    | System requires MessagerAttribute.        |
-| BANG1003 | Usage    | Error    | System requires WatchAttribute.           |
-| BANG1004 | Usage    | Warning  | System does not use MessagerAttribute.    |
-| BANG1005 | Usage    | Warning  | System does not use WatchAttribute.       |
-| BANG2001 | Usage    | Error    | Filter attribute expects only components. |
-| BANG2002 | Usage    | Error    | Messager attribute expects only messages. |
-| BANG2003 | Usage    | Error    | Watch attribute expects only components.  |
-| BANG3001 | Usage    | Error    | Classes cannot be messages.               |
-| BANG3002 | Usage    | Error    | Messages must be declared as readonly.    |
+Rule ID | Category | Severity | Notes
+--------|----------|----------|--------------------
+BANG0001 | Usage    | Error    | Classes cannot be components.             
+BANG0002 | Usage    | Error    | Components must be declared as readonly.  
+BANG1001 | Usage    | Error    | System requires FilterAttribute.          
+BANG1002 | Usage    | Error    | System requires MessagerAttribute.        
+BANG1003 | Usage    | Error    | System requires WatchAttribute.           
+BANG1004 | Usage    | Warning  | System does not use MessagerAttribute.    
+BANG1005 | Usage    | Warning  | System does not use WatchAttribute.       
+BANG2001 | Usage    | Error    | Filter attribute expects only components. 
+BANG2002 | Usage    | Error    | Messager attribute expects only messages. 
+BANG2003 | Usage    | Error    | Watch attribute expects only components.  
+BANG3001 | Usage    | Error    | Classes cannot be messages.               
+BANG3002 | Usage    | Error    | Messages must be declared as readonly.    
--- src/Bang.Analyzers/DiagnosticIds.cs
+++ src/Bang.Analyzers/DiagnosticIds.cs
@@ -7,13 +7,13 @@ public static class Diagnostics
         public static class ClassesCannotBeComponents
         {
             public const string Id = "BANG0001";
-            public const string Message = "Classes cannot be components.";
+            public const string Message = "Classes cannot be components";
         };
 
         public static class ComponentsMustBeReadonly
         {
             public const string Id = "BANG0002";
-            public const string Message = "Components must be declared as readonly.";
+            public const string Message = "Components must be declared as readonly";
         }
     }
 
@@ -22,31 +22,31 @@ public static class Diagnostics
         public static class FilterAttribute
         {
             public const string Id = "BANG1001";
-            public const string Message = "System requires FilterAttribute.";
+            public const string Message = "System requires FilterAttribute";
         }
 
         public static class MessagerAttribute
         {
             public const string Id = "BANG1002";
-            public const string Message = "System requires MessagerAttribute.";
+            public const string Message = "System requires MessagerAttribute";
         }
 
         public static class WatchAttribute
         {
             public const string Id = "BANG1003";
-            public const string Message = "System requires WatchAttribute.";
+            public const string Message = "System requires WatchAttribute";
         }
 
         public static class NonApplicableMessagerAttribute
         {
             public const string Id = "BANG1004";
-            public const string Message = "System does not use MessagerAttribute.";
+            public const string Message = "System does not use MessagerAttribute";
         }
 
         public static class NonApplicableWatchAttribute
         {
             public const string Id = "BANG1005";
-            public const string Message = "System does not use WatchAttribute.";
+            public const string Message = "System does not use WatchAttribute";
         }
     }
 
@@ -55,19 +55,19 @@ public static class Diagnostics
         public static class NonComponentsOnFilterAttribute
         {
             public const string Id = "BANG2001";
-            public const string Message = "Filter attribute expects only components.";
+            public const string Message = "Filter attribute expects only components";
         }
 
         public static class NonMessagesOnMessagerAttribute
         {
             public const string Id = "BANG2002";
-            public const string Message = "Messager attribute expects only messages.";
+            public const string Message = "Messager attribute expects only messages";
         }
 
         public static class NonComponentsOnWatchAttribute
         {
             public const string Id = "BANG2003";
-            public const string Message = "Watch attribute expects only components.";
+            public const string Message = "Watch attribute expects only components";
         }
     }
 
@@ -76,13 +76,13 @@ public static class Diagnostics
         public static class ClassesCannotBeMessages
         {
             public const string Id = "BANG3001";
-            public const string Message = "Classes cannot be messages.";
+            public const string Message = "Classes cannot be messages";
         }
 
         public static class MessagesMustBeReadonly
         {
             public const string Id = "BANG3002";
-            public const string Message = "Messages must be declared as readonly.";
+            public const string Message = "Messages must be declared as readonly";
         }
     }
 }
