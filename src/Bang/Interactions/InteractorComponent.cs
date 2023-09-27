24c27b6b65e19e50fe696e58020aec3e7544e649
bonnibel <isaecia@gmail.com>
2023-09-27 00:35:56 -0700

    Move interactor to Murder.

--- src/Bang/Interactions/InteractorComponent.cs
+++
@@ -1,9 +0,0 @@
-﻿using Bang.Components;
-
-namespace Bang.Interactions
-{
-    /// <summary>
-    /// Component used to signal that an entity is able to interact with other objects.
-    /// </summary>
-    public readonly struct InteractorComponent : IComponent { }
-}
