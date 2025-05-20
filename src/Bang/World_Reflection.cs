0a03a37b37eec595b816b4fb8bc3cd3f8b119440
bonnibel <isaecia@gmail.com>
2025-05-20 00:17:00 -0700

    Support initializing components lookup.

--- src/Bang/World_Reflection.cs
+++ src/Bang/World_Reflection.cs
@@ -12,46 +12,62 @@ namespace Bang
         /// <summary>
         /// Cache the lookup implementation for this game.
         /// </summary>
-        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)]
-        private static Type? _cachedLookupImplementation = null;
+        private static ComponentsLookup? _lookupImplementation = null;
+
+        public static void InitializeLookupComponents(ComponentsLookup lookup)
+        {
+            _lookupImplementation = lookup;
+        }
 
         /// <summary>
         /// Look for an implementation for the lookup table of components.
         /// </summary>
-        [UnconditionalSuppressMessage("AOT", "IL2026:System.Reflection.Assembly.GetTypes() can break functionality when trimming application code. Types might be removed.", Justification = "Target assemblies scanned are not trimmed.")]
-        [UnconditionalSuppressMessage("AOT", "IL2074:Public constructor might have been removed when scanning the candidate type.", Justification = "Target assemblies scanned are not trimmed.")]
         public static ComponentsLookup FindLookupImplementation()
         {
-            if (_cachedLookupImplementation is null)
+            if (_lookupImplementation is not null)
             {
-                Type lookup = typeof(ComponentsLookup);
+                return _lookupImplementation;
+            }
 
-                var isLookup = (Type t) => !t.IsInterface && !t.IsAbstract && lookup.IsAssignableFrom(t);
+            _lookupImplementation = SlowFetchComponentsLookupImplementation();
+            if (_lookupImplementation is null)
+            {
+                throw new InvalidOperationException("A generator is required to be run before running the game!");
+            }
 
-                // We might find more than one lookup implementation, when inheriting projects with a generator.
-                List<Type> candidateLookupImplementations = [];
+            return _lookupImplementation;
+        }
 
-                Assembly[] allAssemblies = AppDomain.CurrentDomain.GetAssemblies();
-                foreach (Assembly s in allAssemblies)
+        [UnconditionalSuppressMessage("AOT", "IL2026:System.Reflection.Assembly.GetTypes() can break functionality when trimming application code. Types might be removed.", Justification = "Target assemblies scanned are not trimmed.")]
+        [UnconditionalSuppressMessage("AOT", "IL2072:Public constructor might have been removed when scanning the candidate type.", Justification = "Target assemblies scanned are not trimmed.")]
+        private static ComponentsLookup? SlowFetchComponentsLookupImplementation()
+        {
+            Type lookup = typeof(ComponentsLookup);
+
+            var isLookup = (Type t) => !t.IsInterface && !t.IsAbstract && lookup.IsAssignableFrom(t);
+
+            // We might find more than one lookup implementation, when inheriting projects with a generator.
+            List<Type> candidateLookupImplementations = [];
+
+            Assembly[] allAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (Assembly s in allAssemblies)
+            {
+                foreach (Type t in s.GetTypes())
                 {
-                    foreach (Type t in s.GetTypes())
+                    if (isLookup(t))
                     {
-                        if (isLookup(t))
-                        {
-                            candidateLookupImplementations.Add(t);
-                        }
+                        candidateLookupImplementations.Add(t);
                     }
                 }
-
-                _cachedLookupImplementation = candidateLookupImplementations.MaxBy(NumberOfParentClasses);
             }
 
-            if (_cachedLookupImplementation is not null)
+            Type? result = candidateLookupImplementations.MaxBy(NumberOfParentClasses);
+            if (result is not null)
             {
-                return (ComponentsLookup)Activator.CreateInstance(_cachedLookupImplementation)!;
+                return Activator.CreateInstance(result) as ComponentsLookup;
             }
 
-            throw new InvalidOperationException("A generator is required to be run before running the game!");
+            return null;
 
             static int NumberOfParentClasses(Type type)
                 => type.BaseType is null ? 0 : 1 + NumberOfParentClasses(type.BaseType);
